f4052ca979a6d32d9a4a9371c49fa4c11fa8fe4e
K.Verameichyk <kateiiverem@gmail.com>
2021-12-13 13:54:13 +0300

    fix: change form for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :login, presence: true, length: {minimum: 10, maximum: 100}
+    :recoverable, :rememberable, :validatable
+  validates :email, presence: true, length: {minimum: 10, maximum: 100}
   validates :password, presence: true, length: {minimum: 6, maximum: 100}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "persons/profile", as: "user_root"
   devise_for :users
   resources :users
   root "pages#home"
