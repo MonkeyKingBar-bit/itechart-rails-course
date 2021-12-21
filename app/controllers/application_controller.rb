b7c4122d9f51de1369f5598e307fd6552f86bd58
eugene <yauheni.papalamau@itechart-group.com>
2021-12-21 11:28:08 +0300

    Merge branch 'master' of /home/yauheni.papalamau/dev/cls/MonkeyKingBar-bit/itechart-rails-course with conflicts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello World!'
+  before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+
+  def render_not_found
+    render 'public/404,html.erb'
   end
 end
---
+++ spec/requests/person_request_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Person', type: :request do
+  context 'test' do
+    it 'ztest' do
+      get :new
+    end
+  end
+
+
+  post people_path,
+       params: { person: { first_name: 'John', last_name: 'Traver', description: 'husband' } }
+
+  expect(response.body).to include 'Person was successfully created.'
+  expect(response.status).to have_http_status(200)
+end
+
+RSpec.describe 'Person with authentication', type: :request do
+  get users_path
+
+  session['user_id'] = 1
+  post people_path,
+       params: { person: { first_name: 'John', last_name: 'Traver', description: 'husband' } }
+
+  expect(response.body).to include 'Person was successfully created.'
+  expect(response.status).to have_http_status(200)
+end
