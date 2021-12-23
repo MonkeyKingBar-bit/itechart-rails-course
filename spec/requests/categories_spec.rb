e3c208b80006e1596a0b7aa36084402e6c296d89
K.Verameichyk <kateiiverem@gmail.com>
2021-12-23 21:02:17 +0300

    fix: check data with rubocop

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,115 +1,24 @@
  require 'rails_helper'
 
-RSpec.describe "/categories", type: :request do
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+RSpec.describe Category, type: :request do
+  context 'Category has in db' do
+    it 'should create a new category and redirect to private account' do
+      get  '/users/sign_in'
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+      post '/categories',
+           params: { category: { title: 'Car', transaction_type: 'Credit'} }
 
-  describe "GET /index" do
-    it "renders a successful response" do
-      Category.create! valid_attributes
-      get categories_path
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      category = Category.create! valid_attributes
-      get category_url(category)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_category_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "render a successful response" do
-      category = Category.create! valid_attributes
-      get edit_category_url(category)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Category" do
-        expect {
-          post categories_url, params: { category: valid_attributes }
-        }.to change(Category, :count).by(1)
-      end
-
-      it "redirects to the created category" do
-        post categories_url, params: { category: valid_attributes }
-        expect(response).to redirect_to(category_url(Category.last))
-      end
+      expect(response).to have_http_status(302)
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Category" do
-        expect {
-          post categories_url, params: { category: invalid_attributes }
-        }.to change(Category, :count).by(0)
-      end
+    it 'should create person with authentication' do
+      get '/users/sign_in'
 
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post categories_url, params: { category: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
+      session['user_id'] = 1
+      post '/categories',
+           params: { category: { title: 'Car', transaction_type: 'Credit'} }
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested category" do
-        category = Category.create! valid_attributes
-        patch category_url(category), params: { category: new_attributes }
-        category.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the category" do
-        category = Category.create! valid_attributes
-        patch category_url(category), params: { category: new_attributes }
-        category.reload
-        expect(response).to redirect_to(category_url(category))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        category = Category.create! valid_attributes
-        patch category_url(category), params: { category: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE /destroy" do
-    it "destroys the requested category" do
-      category = Category.create! valid_attributes
-      expect {
-        delete category_url(category)
-      }.to change(Category, :count).by(-1)
-    end
-
-    it "redirects to the categories list" do
-      category = Category.create! valid_attributes
-      delete category_url(category)
-      expect(response).to redirect_to(categories_url)
+      expect(response).to have_http_status(302)
     end
   end
 end
